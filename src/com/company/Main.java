72eec8450415a6a1cfb756c5f37a2b7392adc3af
Diego Santiago <diegosantiago@knights.ucf.edu>
2021-09-13 06:41:36 -0400

    Exercise 5

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,18 +10,40 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+
         Scanner x = new Scanner(System.in);
-        Scanner y = new Scanner(System.in);
         System.out.print("What is the first number? ");
-        int n1 = x.nextInt();
+        String s1 = x.nextLine();
+        Integer num1 = null;
+        try {
+            num1 = Integer.valueOf(s1);
+
+        } catch (NumberFormatException exception) {
+            System.out.print("Input only integers please.");
+            System.exit(0);
+        }
         System.out.print("What is the second number? ");
-        int n2 = x.nextInt();
+        String s2 = x.nextLine();
+        Integer num2 = null;
+        try {
+            num2 = Integer.valueOf(s2);
 
-        int add = n1+n2;
-        int sub = n1-n2;
-        int mul = n1*n2;
-        int div = n1/n2;
+        } catch (NumberFormatException exception) {
+            System.out.print("Input only integers please.");
+            System.exit(0);
+        }
 
-        System.out.println(n1+" + "+n2+" = "+add+"\r\n"+n1+" - "+n2+" = "+sub+"\r\n"+n1+" * "+n2+" = "+mul+"\r\n"+n1+" / "+n2+" = "+div);
+        int n1 = num1;
+        int n2 = num2;
+
+
+        int add = n1 + n2;
+        int sub = n1 - n2;
+        int mul = n1 * n2;
+        int div = n1 / n2;
+
+        System.out.println(n1 + " + " + n2 + " = " + add + "\r\n" + n1 + " - " + n2 + " = " + sub + "\r\n" + n1 + " * " + n2 + " = " + mul + "\r\n" + n1 + " / " + n2 + " = " + div);
+
+        System.exit(0);
     }
 }
